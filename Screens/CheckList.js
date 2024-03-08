aa48f6aaf40cd5affafefddc4df006329c4929af
Cody <codypschool@gmail.com>
2024-03-07 20:25:13 -0500

    Added functionalities to checklist screen
    I made it so when you create an item in the checklist screen, the name and quantity you enter in the modal actually show up as the name and quantity of the item created. I also added increment and decrement quantity for the items on the checklist, and it works the same way as the increment and decrement quantity for the food screens.

--- Screens/CheckList.js
+++ Screens/CheckList.js
@@ -2,17 +2,50 @@ import {React} from 'react'
 import {View, Text, StyleSheet, Button, TextInput, Alert, Modal} from "react-native";
 import { useState } from 'react';
 
+
 export default function CheckList() {
     
     const[items, setItems] = useState([]);
     const[modalVisible, setModalVisible] = useState(false);
+    const [name,setName] = useState('');
+    const[quantity,setQuantity] = useState(0);
 
-
-    const generateButtons = () => {
+    const generateButtons = (newName,newQuantity) => {
         setModalVisible(false);
-        const newItems = { id: Math.random().toString()};
+        const newItems = { id: Math.random().toString(), name: newName, qty: parseInt(newQuantity)};
         setItems(currentItems => [...currentItems, newItems]);
     }
+    const handleSubtraction = (id) => {
+      const currentItems = items.map((item)=> {
+          if (item.id === id) {
+              return {
+                  name: item.name,
+                  id: item.id,
+                  qty: item.qty-1
+              }
+          }
+          else {
+              return item
+          }
+      });
+      setItems(currentItems);
+  }
+
+  const handleAddition = (id) => {
+      const currentItems = items.map((item)=> {
+          if (item.id === id) {
+              return {
+                  name: item.name,
+                  id: item.id,
+                  qty: item.qty+1
+              }
+          }
+          else {
+              return item
+          }
+      });
+      setItems(currentItems);
+  }
 
     const deleteItem = (id) => {
         setItems(currentItems => currentItems.filter(items => items.id !== id));
@@ -24,32 +57,17 @@ export default function CheckList() {
         return items.map((items,index) => (
           <View key={items.id} style = {styles.buttonGroup}>
             <View>
-                <Text>Item #{index + 1}</Text> 
+                <Text>{items.name}</Text> 
+                <Text style={{fontSize: 20}}>{items.qty}</Text>
             </View>
-            <Button title="+"></Button>
-            <Button title="-"/>
+            <Button title="+" onPress={()=> handleAddition(items.id)} />
+            <Button title="-" onPress={()=> handleSubtraction(items.id)} />
             <Button title="Delete Item" onPress={() => deleteItem(items.id)} />
           </View>
         ));
     };
 
     return(
-        //  <View style = {styles.container}>
-        //     <Button title = "Add Item" onPress={() => setModalVisible(true)}></Button>
-        //     {renderItemButtons()}
-        //     <Modal
-        //         visible={modalVisible}
-        //         animationType="slide"
-        //         transparent={true}
-        //     >
-        //         <View style={styles.modalView}>
-        //             <TextInput placeholder="Enter username"/>
-        //             <TextInput placeholder = "Enter Quantity" />
-        //             <Button title="Add Item" onPress={generateButtons} />
-        //         </View>
-        //     </Modal>
-            
-        // </View>
         <View style = {styles.container}>
             <Button title = "Add Item" onPress= {() => setModalVisible(true)}></Button>
             {renderItemButtons()}
@@ -59,9 +77,17 @@ export default function CheckList() {
                 transparent={true}
             >
                 <View style={styles.modalView}>
-                  <TextInput style = {styles.moodalInput} placeholder="Enter item name:" placeholderTextColor= "#000"/>
-                  <TextInput style = {styles.moodalInput} placeholder = "Enter quantity" placeholderTextColor= "#000"/> 
-                  <Button title="Add Item" onPress={generateButtons} />
+                  <TextInput 
+                  style = {styles.moodalInput} 
+                  placeholder="Enter item name:" 
+                  onChangeText={(name)=> setName(name)}
+                  placeholderTextColor= "#000"/>
+                  <TextInput 
+                  style = {styles.moodalInput} 
+                  placeholder = "Enter quantity" 
+                  onChangeText={(quantity)=> parseInt(setQuantity(quantity))}
+                  placeholderTextColor= "#000"/> 
+                  <Button title="Add Item" onPress={()=>generateButtons(name,quantity)} />
                 </View>
             </Modal>
             
